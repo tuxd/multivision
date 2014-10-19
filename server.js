26947e3eaa8584cf7018a3a15761f4014f8e9e41
Daniel Jones <daniel@poweredbybees.com>
2014-10-19 17:14:17 +0700

    Added setup files

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.set('views', __dirname + '.server/views');
+app.set('view engine', 'jade');
+
+app.get('*', function (req, res) {
+    res.render('index');
+});
